3eabc916bbd9e36f0f9c196febc596de3530f84e
mcbryan1 <mcbryansolomon@yahoo.com>
2022-11-14 17:45:58 +0000

    wip: done with sign in

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,3 +3,5 @@
 
 export { default as logo } from './images/logo.png';
 export {default as banner} from './images/banner.jpg'
+export {default as banner2} from './images/banner2.png'
+export {default as person} from './images/person.png'
--- src/pages/auth/login/Login.jsx
+++ src/pages/auth/login/Login.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import { banner2, logo, person } from "../../../assets";
+import { Navbar } from "../../../components";
+import "./Login.css";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <>
+    <Navbar/>
+    <div className="container">
+      <div className="left-container">
+        <div className="auth-container">
+          <div className="auth-form">
+            <h1>Sign In</h1>
+            <h3>Create Account to start using EasyRecruit</h3>
+            <form className="form">
+              <div className="form-group">
+                <div>
+                  <label htmlFor="email">Email</label>
+                  <br />
+                  <input type="email" name="email" id="email" />
+                </div>
 
-export default Login
+                <div className="password-container">
+                  <label htmlFor="password">Password</label>
+                  <br />
+                  <input type="password" name="password" id="password" />
+                </div>
+
+                <div className="forgot-password-container">
+                  <a href="#">Forgot Password?</a>
+                </div>
+                <button type="submit">Sign In</button>
+                <div className="dont-have-container">
+                  <p>Don't have an account?</p>
+                  <Link to="/signup" >
+                    <a>Sign Up</a>
+                  </Link>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <div className="right-container">
+        <img src={person} alt="banner" />
+      </div>
+    </div>
+    </>
+  );
+};
+
+export default Login;
